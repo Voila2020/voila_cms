15c1caf139acd7db17456e53cd6bcd390cd3521d
Lama Youzghatli <lama@voila.digital>
2023-06-24 14:02:59 +0300

    Delete PrivilegesController

--- src/database/seeds/CBSeeder.php
+++ src/database/seeds/CBSeeder.php
@@ -88,7 +88,7 @@ class CBSeeder extends Seeder
                 'icon' => 'fa fa-cog',
                 'path' => 'privileges_roles',
                 'table_name' => 'cms_privileges_roles',
-                'controller' => 'PrivilegesRolesController',
+                'controller' => 'PrivilegesController',
                 'is_protected' => 1,
                 'is_active' => 1,
             ],
