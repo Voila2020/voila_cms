b29d3b2b57953b87e77effdc9c0f8c3f8558ff6f
leenvoila <leen@voila.digital>
2023-07-11 13:33:46 +0300

    Fix bug in the custom Route .

--- src/routes.php
+++ src/routes.php
@@ -1,11 +1,11 @@
 <?php
 
 use crocodicstudio\crudbooster\helpers\CBRouter;
-use crocodicstudio\crudbooster\middlewares\CBAuthAPI;
+use crocodicstudio\crudbooster\middlewares\CBBackend;
 
 
 CBRouter::route();
-Route::group(['middleware' => ['api', CBAuthAPI::class], 'namespace' => 'App\Http\Controllers'], function () {
+Route::group(['middleware' => ['web', CBBackend::class], 'namespace' => 'App\Http\Controllers'], function () {
 
     Route::get('{url}', [App\Http\Controllers\LandingPagesController::class, 'catchView']);
 });
