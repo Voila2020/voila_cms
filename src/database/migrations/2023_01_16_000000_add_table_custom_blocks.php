c42fdb1c8d6193d86c774f4e5a531f24f1862512
leenvoila <leen@voila.digital>
2023-07-15 11:38:42 +0300

    Fix bug in the custom blocks migration file.

--- src/database/migrations/2023_01_16_000000_add_table_custom_blocks.php
+++ src/database/migrations/2023_01_16_000000_add_table_custom_blocks.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddTableFormField extends Migration
+class AddTableCustomBlocks extends Migration
 {
     /**
      * Run the migrations.
