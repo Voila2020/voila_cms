dc919e6ffbbc739fa06e3897f6115646bdbaa46d
Lama Youzghatli <lama@voila.digital>
2023-07-29 11:43:31 +0300

    Delete Landing Page Route

--- src/routes.php
+++ src/routes.php
@@ -5,7 +5,7 @@ use crocodicstudio\crudbooster\middlewares\CBBackend;
 
 
 CBRouter::route();
-Route::group(['middleware' => ['web', CBBackend::class], 'namespace' => 'App\Http\Controllers'], function () {
+// Route::group(['middleware' => ['web', CBBackend::class], 'namespace' => 'App\Http\Controllers'], function () {
 
-    Route::get('{url}', [App\Http\Controllers\LandingPagesController::class, 'catchView']);
-});
+//     Route::get('{url}', [App\Http\Controllers\LandingPagesController::class, 'catchView']);
+// });
