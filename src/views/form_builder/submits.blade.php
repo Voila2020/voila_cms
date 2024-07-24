34ba181180321f220c374cfc41437403c1244a03
mhd_joma <mhd.jumaa@voila.digital>
2024-07-24 09:42:49 +0300

    fix style

--- src/views/form_builder/submits.blade.php
+++ src/views/form_builder/submits.blade.php
@@ -2,27 +2,29 @@
 @section('title', 'submits') @section('content')
 
 <div class="row">
-    <form method='post' target="_blank" action='{{ CRUDBooster::mainpath('export-data?t=' . time()) }}'>
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <input type='hidden' name='filename' class='form-control' required
-            value='Report {{ $module_name }} - {{ date('d M Y') }}' />
-
-        <input type="hidden" name="export_data_columns" value="{{ json_encode($export_data_columns) }}">
-
-        <input type="hidden" name="export_data_result" value="{{ json_encode($export_data_result) }}">
-        <input type="hidden" name="fileformat" value="xls">
-        <div class="mb-4" style="margin-bottom:4px ; margin-left:70px">
-            <button class="btn btn-primary btn-submit " type="submit">Export</button>
-        </div>
-    </form>
     <div class="container">
-            @foreach($data as $item)
-               <div class="well">
-                    <strong>Date: </strong> {{$item->updated_at}}<strong style="margin-left: 20px">IP: </strong> {{$item->ip}}
-                    {!! $item->response !!}
+        @if ($export_data_columns)
+            <form method='post' target="_blank" action='{{ CRUDBooster::mainpath('export-data?t=' . time()) }}'>
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type='hidden' name='filename' class='form-control' required
+                    value='Report {{ $module_name }} - {{ date('d M Y') }}' />
 
-               </div>
-            @endforeach
+                <input type="hidden" name="export_data_columns" value="{{ json_encode($export_data_columns) }}">
+
+                <input type="hidden" name="export_data_result" value="{{ json_encode($export_data_result) }}">
+                <input type="hidden" name="fileformat" value="xls">
+                <div style="margin-bottom: 5px">
+                    <button class="btn btn-primary btn-submit" type="submit">Export</button>
+                </div>
+            </form>
+        @endif
+        @foreach ($data as $item)
+            <div class="well">
+                <strong>Date: </strong> {{ $item->updated_at }}<strong style="margin-left: 20px">IP: </strong>
+                {{ $item->ip }}
+                {!! $item->response !!}
+            </div>
+        @endforeach
     </div>
 </div>
 @endsection
