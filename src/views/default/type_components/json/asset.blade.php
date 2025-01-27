f4e7b266514e98f966ab3329a20683d07fde4683
Lama Youzghatli <lama@voila.digital>
2025-01-27 09:24:59 +0300

    Update

--- src/views/default/type_components/json/asset.blade.php
+++ src/views/default/type_components/json/asset.blade.php
@@ -1,2 +1,2 @@
-<script src='<?php echo asset("vendor/crudbooster/assets/jsoneditor/jsoneditor.min.js")?>'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/10.1.2/jsoneditor.min.js'></script>
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/10.1.2/jsoneditor.min.css' />
