381d320ffab32731fb200d354d55c2f11d8b5280
Haider.Voila.Digital <haider@voila.digital>
2025-09-13 12:38:29 +0300

    make add  hidden type with translation field list

--- src/views/default/type_components/hidden/component.blade.php
+++ src/views/default/type_components/hidden/component.blade.php
@@ -1,1 +1,15 @@
-<input type='hidden' name="{{$name}}" value='{{$value}}'/>
+@if (!@$form['translation'])
+    @if($current_language->default != null && $current_language->default == 1)
+
+    <input type='hidden' name="{{$name}}" value='{{$value}}'/>
+
+    @endif
+@else
+    @php
+        @$value = isset($form['value']) ? $form['value'] : '';
+        @$value = isset($row->{$name. '_' . $current_language->code}) ? $row->{$name. '_' . $current_language->code} : $value;
+        $old = old($name . '_' . $current_language->code);
+        $value = !empty($old) ? $old : $value;
+    @endphp
+    <input type='hidden' name="{{ $name . '_' . $current_language->code }}" value='{{$value}}'/>
+@endif
