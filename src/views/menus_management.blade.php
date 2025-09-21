a35bcebbce48d965c5f6798f8259a9fd76fd33ea
Haider.Voila.Digital <haider@voila.digital>
2025-09-21 12:17:19 +0300

    fix bug in menus management module

--- src/views/menus_management.blade.php
+++ src/views/menus_management.blade.php
@@ -224,6 +224,9 @@
                         action='{{ CRUDBooster::mainpath('add-save') }}'>
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <input type='hidden' name='return_url' value='{{ Request::fullUrl() }}' />
+                        @php 
+                            $lang = $websiteLanguages->first();
+                        @endphp
                         @include('crudbooster::default.form_body')
                         <p align="right"><input type='submit' class='btn btn-primary' value='{{ cbLang('Add Menu') }}' />
                         </p>
